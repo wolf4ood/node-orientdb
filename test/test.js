c984792d879379ee7321cc6e16c77aa639b35c2d
maggiolo00 <maggiolo00@gmail.com>
2011-05-24 22:13:22 +0200

    first commit

---
+++ test/test.js
@@ -0,0 +1,16 @@
+var OrientDB = require("../lib/node-orientdb.js");
+var database = new OrientDB("localhost",2480);
+
+database.on("open",function(chunk){
+	database.load('#5:3');
+	//database.remove('#5:3');
+});
+database.on("remove",function(chunk){
+	console.log(chunk);
+});
+database.on("load",function(chunk){
+	console.log(chunk);
+});
+
+database.open("admin","admin");
+
