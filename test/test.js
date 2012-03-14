bb03de38f70ab2637c54958a4d43fc6376f6a4d8
maggiolo00 <maggiolo00@pengolinbox.(none)>
2012-03-14 21:14:59 +0100

    removed old file and updated all methods

--- test/test.js
+++ test/test.js
@@ -1,16 +1,15 @@
 var OrientDB = require("../lib/orientdb.js");
-var database = new OrientDB("localhost",2480);
+var database = new OrientDB("localhost", 2480);
 
-database.on("open",function(chunk){
+database.on("open", function(chunk) {
 	database.load('#5:3');
-	//database.remove('#5:3');
+	// database.remove('#5:3');
 });
-database.on("remove",function(chunk){
-	console.log(chunk);
-});
-database.on("load",function(chunk){
-	console.log(chunk);
-});
+// database.on("remove",function(chunk){
+// console.log(chunk);
+// });
+// database.on("load",function(chunk){
+// console.log(chunk);
+// });
 
-database.open("admin","admin");
-
+database.open("admin", "admin", "demo");
